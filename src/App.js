db8d0960343890b38bbcac1bb9f505e1336241c2
Dharmit Dosani <dharmitdosani@gmail.com>
2021-09-20 17:56:49 +0530

    header added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./header/header.component";
 import { Switch, Route } from "react-router-dom";
 
 import "./App.css";
 
 const App = () => {
   return (
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/shop" component={ShopPage} />
-    </Switch>
+    <>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
+    </>
   );
 };
 
