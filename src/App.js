3509cc7d7a9aed66566a284888f081e1ca9be530
Dharmit Dosani <dharmitdosani@gmail.com>
2021-09-20 18:43:18 +0530

    sign in page structure added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import { Switch, Route } from "react-router-dom";
 
 import "./App.css";
@@ -12,6 +13,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/sign-in" component={SignInAndSignUpPage} />
       </Switch>
     </>
   );
