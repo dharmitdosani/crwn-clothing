78c447293a1d66fa440e64cf2bab453aacc7063b
Dharmit Dosani <dharmitdosani@gmail.com>
2021-09-20 13:25:33 +0530

    modified styling for homepage using scss instead of styled components

--- src/homepage.component.js
+++ src/homepage.component.js
@@ -1,50 +1,43 @@
 import React from "react";
-import {
-  HomePageContainer,
-  DirectoryMenu,
-  MenuItem,
-  Content,
-  Title,
-  SubTitle,
-} from "./homepage.styles";
 
-const HomePage = () => {
-  return (
-    <HomePageContainer>
-      <DirectoryMenu>
-        <MenuItem>
-          <Content>
-            <Title>HATS</Title>
-            <SubTitle>SHOP NOW</SubTitle>
-          </Content>
-        </MenuItem>
-        <MenuItem>
-          <Content>
-            <Title>JACKETS</Title>
-            <SubTitle>SHOP NOW</SubTitle>
-          </Content>
-        </MenuItem>
-        <MenuItem>
-          <Content>
-            <Title>SNEAKERS</Title>
-            <SubTitle>SHOP NOW</SubTitle>
-          </Content>
-        </MenuItem>
-        <MenuItem>
-          <Content>
-            <Title>WOMENS</Title>
-            <SubTitle>SHOP NOW</SubTitle>
-          </Content>
-        </MenuItem>
-        <MenuItem>
-          <Content>
-            <Title>MENS</Title>
-            <SubTitle>SHOP NOW</SubTitle>
-          </Content>
-        </MenuItem>
-      </DirectoryMenu>
-    </HomePageContainer>
-  );
-};
+import "./homepage.styles.scss";
+
+const HomePage = () => (
+  <div className="homepage">
+    <h1>Welcome to my Homepage</h1>
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <div className="title">HATS</div>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <div className="title">JACKETS</div>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <div className="title">SHOES</div>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <div className="title">WOMENS</div>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <div className="title">MENS</div>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 
 export default HomePage;
