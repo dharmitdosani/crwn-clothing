b13dfd98a65c20db8efdc9af34d23fe8aed431af
Dharmit Dosani <dharmitdosani@gmail.com>
2021-09-20 17:11:38 +0530

    collections and previews added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 import { Switch, Route } from "react-router-dom";
 
 import "./App.css";
-
-const Hats = (props) => {
-  console.log(props);
-  return <div>Hats</div>;
-};
 
 const App = () => {
   return (
     <Switch>
       <Route exact path="/" component={HomePage} />
-      <Route exact path="/hats" component={Hats} />
-      <Route exact path="/men" component={Hats} />
+      <Route exact path="/shop" component={ShopPage} />
     </Switch>
   );
 };
