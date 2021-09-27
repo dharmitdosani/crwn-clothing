a603257ececd96a9fa53a40d0182394332066ec5
Dharmit Dosani <dharmitdosani@gmail.com>
2021-09-27 20:03:35 +0530

    add and remove item func to checkout item component

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -8,3 +8,13 @@ export const addItem = (item) => ({
   type: CART_ACTIONS.ADD_ITEM,
   payload: item,
 });
+
+export const clearItemFromCart = (item) => ({
+  type: CART_ACTIONS.CLEAR_ITEM_FROM_CART,
+  payload: item,
+});
+
+export const removeItem = (item) => ({
+  type: CART_ACTIONS.REMOVE_ITEM,
+  payload: item,
+});
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,9 @@
 import { CART_ACTIONS } from "./cart.constants";
-import { addItemToCart } from "./cart.utils";
+import {
+  addItemToCart,
+  clearItemFromCart,
+  removeItemFromCart,
+} from "./cart.utils";
 
 const INITIAL_STATE = {
   hidden: true,
@@ -18,6 +22,18 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload),
+      };
+
+    case CART_ACTIONS.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: clearItemFromCart(state.cartItems, action.payload),
+      };
+
+    case CART_ACTIONS.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload),
       };
 
     default:
